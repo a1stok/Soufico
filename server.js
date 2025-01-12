a1b5a05198fed42f74e40cf5b58595d61f5d705d
Kostia Novosydliuk <knovosyd@uwaterloo.ca>
2025-01-12 09:29:18 -0500

    added cors which where accidentally deleted

--- server.js
+++ server.js
@@ -7,15 +7,14 @@ const userRoutes = require("./routes/userRoutes");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 0; 
 
-app.use(express.json());
-
+app.use(express.json()); 
 app.use(
   cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: "*", 
+    methods: ["GET", "POST", "PUT", "DELETE"], 
+    allowedHeaders: ["Content-Type", "Authorization"], 
   })
 );
 
@@ -31,4 +30,11 @@ app.use((err, req, res, next) => {
 const server = app.listen(PORT, "0.0.0.0", () => {
   const assignedPort = server.address().port;
   console.log(`Server is running on http://0.0.0.0:${assignedPort}`);
+});
+
+process.on("SIGTERM", () => {
+  console.log("Server is shutting down...");
+  server.close(() => {
+    console.log("Server has been closed.");
+  });
 });
