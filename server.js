13d0648e57003ea69f5f7e99d0214a676b2d6f03
Kostia Novosydliuk <knovosyd@uwaterloo.ca>
2025-01-12 09:45:10 -0500

    updated server cuz was getting Cannot GET / error

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path"); 
 const { connectToDB } = require("./db");
 const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 0; 
+const PORT = process.env.PORT || 0;
 
-app.use(express.json()); 
+app.use(express.json());
 app.use(
   cors({
-    origin: "*", 
-    methods: ["GET", "POST", "PUT", "DELETE"], 
-    allowedHeaders: ["Content-Type", "Authorization"], 
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
 connectToDB();
 
 app.use("/api/users", userRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  const buildPath = path.join(__dirname, "build");
+  app.use(express.static(buildPath));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(buildPath, "index.html"));
+  });
+}
 
 app.use((err, req, res, next) => {
   console.error("Unhandled error:", err.message);
