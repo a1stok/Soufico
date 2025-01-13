7b84acbe17e895b9d46739f0bfc6e35113ade739
Kostia Novosydliuk <knovosyd@uwaterloo.ca>
2025-01-12 23:54:35 -0500

    index.js back to normal, was confused cuz of stripe

--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
-import PaymentPage from "./pages/PaymentPage";
-import ShopPage from "./pages/ShopPage"; 
-import MyAccountPage from "./pages/MyAccountPage"; 
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/shop" element={<ShopPage />} />
-        <Route path="/payment" element={<PaymentPage />} /> 
-        <Route path="/my-account" element={<MyAccountPage />} /> 
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>
 );
