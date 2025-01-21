c19ca19a4ad1e6005bcc8bf2a34d8100daee975c
Kostia Novosydliuk <knovosyd@uwaterloo.ca>
2025-01-20 22:44:55 -0500

    simple home page done

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './HomePage.css';
+import catImage from '../images/cat.jpg'; 
 
 const HomePage = () => {
   return (
-    <div className="hero">
-      <div className="hero-text">
-        <h1>Welcome to FlowerShop</h1>
-        <p>ААХАХАХАХАХАХАХАХАХАХАХАХАХАХАХАХАХАХ</p>
-        <button className="hero-button">Shop Now</button>
+    <div className="home-container">
+      <h1>Welcome to Soufico</h1>
+      <p>Your custom Spotify playlists inspired by movies.</p>
+      <div className="home-links">
+        <Link to="/shop">Shop - Browse the selection, not available for purchase yet❤️.</Link>
+        <Link to="/subscriptions">Film Playlist - Generate movie-inspired playlists.</Link>
+        <Link to="/my-playlist">My Playlist - Manage your saved Spotify playlists.</Link>
+        <Link to="/about">About - Learn more about us.</Link>
+        <Link to="/my-account">My Account - Access your profile and features.</Link>
+        <Link to="/login">Login - Log in to access personalized features.</Link>
+      </div>
+      <div className="cat-image-container">
+        <img src={catImage} alt="Cute Cat" className="mereal" />
       </div>
     </div>
   );
