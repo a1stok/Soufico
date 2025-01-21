2dfecfea42ac64c67b70f1d7d905f9b2badc3da3
Kostia Novosydliuk <knovosyd@uwaterloo.ca>
2025-01-20 23:18:19 -0500

    translation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import Navbar from "./components/Navbar/Navbar";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
@@ -10,26 +11,39 @@ import SubscriptionsPage from "./pages/SubscriptionsPage";
 import SpotifyCallback from "./pages/SpotifyCallback";
 import MyAccountPage from "./pages/MyAccountPage";
 import MovieDetailsPage from "./pages/MovieDetailsPage";
-import MyCollectionPage from "./pages/MyCollectionPage"; // New My Collection page
+import MyCollectionPage from "./pages/MyCollectionPage"; 
 import CheckoutForm from "./components/CheckoutForm";
+import "./App.css"; // Import for transition styles
+
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <TransitionGroup>
+      <CSSTransition key={location.key} classNames="fade" timeout={300}>
+        <Routes location={location}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/shop" element={<ShopPage />} />
+          <Route path="/my-account" element={<MyAccountPage />} />
+          <Route path="/subscriptions" element={<SubscriptionsPage />} />
+          <Route path="/my-playlist" element={<MyCollectionPage />} />
+          <Route path="/movie/:id" element={<MovieDetailsPage />} />
+          <Route path="/callback" element={<SpotifyCallback />} />
+          <Route path="/checkout" element={<CheckoutForm />} />
+        </Routes>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+}
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/shop" element={<ShopPage />} />
-        <Route path="/my-account" element={<MyAccountPage />} />
-        <Route path="/subscriptions" element={<SubscriptionsPage />} />
-        <Route path="/my-playlist" element={<MyCollectionPage />} /> {/* Added My Playlist route */}
-        <Route path="/movie/:id" element={<MovieDetailsPage />} />
-        <Route path="/callback" element={<SpotifyCallback />} />
-        <Route path="/checkout" element={<CheckoutForm />} />
-      </Routes>
+      <AnimatedRoutes />
     </Router>
   );
 }
